bb36d99828d4f6c4fb2d3369b01d19e1a0cdbe29
Zach Arnold <me@zacharnold.org>
2015-06-03 08:30:47 -0400

    adds node-restful package

--- app.js
+++ app.js
@@ -57,20 +57,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
-  res.json({message: 'hooray! welcome to our api!'});
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
 
 module.exports = app;
